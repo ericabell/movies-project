8d7114d7dd31665f8da4eff913776bb36991a757
Eric Abell <ericabell@users.noreply.github.com>
2017-09-25 15:01:17 -0500

    Initial commit

---
+++ src/main/java/com/example/moviesproject/MoviesProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.moviesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesProjectApplication.class, args);
+	}
+}
